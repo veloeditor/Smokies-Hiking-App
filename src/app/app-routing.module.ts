5ffae7fe2cefda45483e3b62ccec2cf49d68464d
Brian Wilson <wilsonedit@gmail.com>
2020-07-17 13:10:22 -0500

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HikesListComponent } from './components/hikes-list/hikes-list.component';
+import { TrailsLogComponent } from './components/trails-log/trails-log.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'hikes', component: HikesListComponent },
+  { path: 'trails', component: TrailsLogComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

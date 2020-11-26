413b9f92748a61448720ed1d767a397f62e7bd61
Brian Wilson <brianwilson@Brians-MacBook-Air.local>
2020-11-26 13:57:23 -0600

    list now refreshes after submitting, need to refactor to private method and call in submit and ngOnInit

--- src/app/components/trails-log/trails-log.component.ts
+++ src/app/components/trails-log/trails-log.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Trail } from '../../interfaces/trail';
 
@@ -42,9 +42,9 @@ export class TrailsLogComponent implements OnInit {
         return acc + Number(userHike.totalMiles);
       }, 0);
       this.userUniqueMilesHiked = miles.toFixed(1);
+      console.log('ngOnInit fires');
     });
 
-    // ToDo: put call to trailsService to retrieve all trails and set equal to trails for auto-complete
     this.trailsService.getAllTrails().subscribe((data: any[]) => {
       this.trails = data;
       console.log(this.trails);
@@ -58,9 +58,6 @@ export class TrailsLogComponent implements OnInit {
       sections: this.sectionNameArray,
     });
 
-    // ToDo: probably need to move logic for determining length of hike out of here and
-    // move to line 82 where we are looking at the valueChange of the sections form field
-
     this.trailForm.controls.trailName.valueChanges.subscribe((change) => {
       const trailObjSelected = this.trails?.find((t) => t.name === change);
       if (trailObjSelected?.sections?.length < 1) {
@@ -71,10 +68,6 @@ export class TrailsLogComponent implements OnInit {
         }
       } else {
         this.sectionNameArray = trailObjSelected?.sections;
-        this.trailObjSelectedMiles = 0;
-        this.sectionNameArray.forEach((section) => {
-          this.trailObjSelectedMiles += section.sectionLength;
-        });
       }
     });
 
@@ -83,24 +76,45 @@ export class TrailsLogComponent implements OnInit {
     });
 
     this.trailForm.controls.sections.valueChanges.subscribe((value) => {
-      console.log('sections', value[0].sectionLength);
+      let selectedSection = [];
+      selectedSection = value;
+      const miles = selectedSection.reduce((acc, section) => {
+        return acc + Number(section.sectionLength);
+      }, 0);
+      this.trailObjSelectedMiles = miles.toFixed(1);
+      // this.trailObjSelectedMiles += value[0]?.sectionLength;
+      // this.trailObjSelectedMiles.toFixed(1);
+      console.log(miles);
     });
+  }
+
+  get sections(): FormControl {
+    return this.trailForm.get('sections') as FormControl;
   }
 
   saveTrail(trailForm: FormGroup): void {
     const hike = {
       trailName: this.trailForm.value.trailName,
-      totalMiles: this.trailForm.value.totalMiles,
+      totalMiles: this.trailObjSelectedMiles,
       date: this.trailForm.value.date,
       comments: this.trailForm.value.comments,
       sections: this.trailForm.value.sections
     } as UserHike;
 
-    this.userHikesService.postHike(hike).subscribe(data => {
-      console.log(data);
+    this.userHikesService.postHike(hike).subscribe(_ => {
+    });
+    this.userHikesService.getAllUserHikes().subscribe((data: any[]) => {
+      this.userHikes = data;
+      this.userHikes.sort((a, b) =>
+        new Date(a?.date) > new Date(b?.date) ? -1 : 1
+      );
+      const miles = this.userHikes.reduce((acc, userHike) => {
+        return acc + Number(userHike.totalMiles);
+      }, 0);
+      this.userUniqueMilesHiked = miles.toFixed(1);
+      console.log('ngOnInit fires');
     });
 
-    this.userHikesService.getAllUserHikes();
   }
 
   openForm() {

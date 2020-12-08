e8010248b150eb8ad34241061b27b938727e796f
Brian Wilson <wilsonedit@gmail.com>
2020-12-08 10:30:32 -0600

    started implementing edit form with needed logic. in progress

--- src/app/components/hike-form/hike-form.component.ts
+++ src/app/components/hike-form/hike-form.component.ts
@@ -1,5 +1,12 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 import { Component, Input, OnInit } from '@angular/core';
 import { UserHike } from 'src/app/interfaces/user-hike';
+import { Trail } from '../../interfaces/trail';
+import { UserHikesService } from 'src/app/services/user-hikes.service';
+import { TrailsService } from 'src/app/services/trails.service';
+
 
 @Component({
   selector: 'app-hike-form',
@@ -10,7 +17,19 @@ export class HikeFormComponent implements OnInit {
 
   @Input() userHike: UserHike;
 
-  constructor() { }
+  trailForm: FormGroup;
+  filteredTrails: Observable<Trail[]>;
+  trailObjSelectedMiles = 0;
+  sectionNameArray: [{sectionName: string, sectionLength: number}];
+  selectedSection = [];
+
+
+
+  constructor(
+    private userHikesService: UserHikesService,
+    private fb: FormBuilder,
+    private trailsService: TrailsService
+  ) { }
 
   ngOnInit(): void {
   }
